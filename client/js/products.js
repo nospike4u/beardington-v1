dabd0e576a45aec38ab7b2445f66356c7bc6384f
Dominic Spike <dominic.spike@seibert.group>
2026-02-24 22:54:41 +0100

    Product listings now showing. Images added to database and now rendered in frontend

--- client/js/products.js
+++ client/js/products.js
@@ -1,33 +1,48 @@
-console.log('Loaded product.js');
+console.log("Loaded product.js");
 export const renderProducts = async () => {
-    const app = document.getElementById('app');
-    
-    app.innerHTML = '<img src="client/assets/moustache-favicon-180x180.png" />';
+  const app = document.getElementById("app");
 
-    try {
- 
-        const response = await fetch('/api/products');
-        const data = await response.json();
+  app.innerHTML = '<img src="client/assets/moustache-favicon-180x180.png" />';
 
-        const productListHTML = data.products.map(product => `
+  try {
+    const response = await fetch("/api/products");
+    const data = await response.json();
+
+    const productListHTML = data.products
+      .map((product) => {
+        let firstUrl = "";
+        try {
+          const urls = JSON.parse(product.image_urls);
+          if (Array.isArray(urls) && urls.length > 0) {
+            firstUrl = urls[0];
+          }
+        } catch (e) {
+          console.log(e.message, "Error. Image not found.");
+        }
+        return `
             <div class="product-card">
-                <img src="${Array.isArray(product.imageUrls) ? product.imageUrls[0] : ''}" alt="${product.name}" width="150">
+                <img src="${firstUrl}" alt="${product.name}" width="150">
                 <h2>${product.name}</h2>
                 <p>${product.description}</p>
                 <span class="price">$${(product.priceCents / 100).toFixed(2)}</span>
+                <div class="add-btn-container">
                 <button class="add-btn" data-id="${product.id}">Add to Cart</button>
+                </div>
+                <div class="view-details-container">
                 <a href="/products/${product.slug}" data-link>View Details</a>
+                </div>
             </div>
-        `).join('');
+        `;
+      })
+      .join("");
 
-        app.innerHTML = `
+    app.innerHTML = `
             <section class="products-grid">
                 <h1>Store</h1>
                 <div class="grid">${productListHTML}</div>
             </section>
         `;
-
-    } catch (error) {
-        app.innerHTML = `<p class="error">Failed to load products: ${error.message}</p>`;
-    }
+  } catch (error) {
+    app.innerHTML = `<p class="error">Failed to load products: ${error.message}</p>`;
+  }
 };
